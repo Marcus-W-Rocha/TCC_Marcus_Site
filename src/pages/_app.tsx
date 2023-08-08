64be6de3fbd2d9171f0fa62a97e8bbc7ade60c9b
Mayron Junior <mayronjunior5@gmail.com>
2023-08-08 18:04:10 -0300

    Resetando Aplicativo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
-import Layout from '@/components/layout/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const isAuth = Component.name === 'Login'
-
-  return (
-    <main>
-      <SessionProvider>
-        <Layout isAuth={isAuth}>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionProvider>
-    </main>
-  )
+  return <Component {...pageProps} />
 }
