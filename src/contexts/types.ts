b37bcf9af4c0c13e758094f1f9226af9d5502301
Mayron Junior <mayronjunior5@gmail.com>
2023-08-08 11:15:34 -0300

    Resetando projeto

--- src/contexts/types.ts
+++
@@ -1,9 +0,0 @@
-export default interface User {
-    nome?: String;
-    token?: String;
-}
-
-export default interface UserContextProps {
-    user?: User | null;
-    setUser: React.Dispatch<React.SetStateAction<User>>;
-}
