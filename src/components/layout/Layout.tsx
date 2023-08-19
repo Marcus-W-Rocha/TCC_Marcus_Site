f46aa8e74a6e5a8e60b67e8df8e0b528a616abc2
Marcus Barros <ma.wel@live.com>
2023-08-19 18:35:48 -0300

    teste

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react"
-import App_Name from "../common/General_Info"
 import { useSession, signOut } from "next-auth/react";
 
 
@@ -13,17 +12,7 @@ export default function Layout({ children }: LayoutProps) {
     return (
         <div>
             <header>
-            <div></div>
 
-                <div className="text-4xl">{App_Name()}</div>
-                {session.status === 'authenticated'?(
-                    <div>
-                        Aqui para o logout
-                        <button onClick={()=>signOut()}>Exit</button>
-                    </div>
-                ):(
-                    <div></div>
-                )}
             </header>
             {children}
         </div>
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,19 +5,12 @@ import Head from "next/head"
 
 const Page404 = () => {
   return (
-    <div>
+    <>
       <Head><title>Página não encontrada</title></Head>
       <div className="flex flex-col sm:flex-row w-full h-screen items-center justify-center">
-        {/* {WalkingIllustration} */}
-        <div className="flex flex-col items-center sm:items-start justify-center ml-0 sm:ml-5 mt-5 sm:mt-0 ">
-          <h1 className="text-6xl font-semibold">404</h1>
-          <p className="text-xl mt-2">Página não encontrada!</p>
-          <Link className="underline mt-5" href='/workspace'>
-            Voltar para a página inicial do Ymir
-          </Link>
-        </div>
+        
       </div>
-    </div>
+    </>
   );
 }
 
